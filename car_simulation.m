a9c255af8a9f3368255c46a5011bff96a72df42a
li-chengkun <48325263+li-chengkun@users.noreply.github.com>
2019-05-12 18:35:01 +0800

    Update car_simulation.m

--- car_simulation.m
+++ car_simulation.m
@@ -1,12 +1,12 @@
-function car
+function car_simulation
 
-function ds=carddt(t,s)
-%Á½ÂÖÖ®¼ä¾àÀë£¬×óÂÖ×ªËÙ£¬ÓÒÂÖ×ªËÙ
+function ds=carddt(~,s)
+%ä¸¤è½®ä¹‹é—´è·ç¦»ï¼Œå·¦è½®è½¬é€Ÿï¼Œå³è½®è½¬é€Ÿ
 global l; global u1; global u2;
 
-l = 1; u1 = 1; u2 = 1;
+l = 1; u1 = 2; u2 = 3;
 x1 = s(1); y1 = s(2); x2 = s(3); y2 = s(4);
-%×´Ì¬·½³Ì
+%çŠ¶æ€æ–¹ç¨‹
 dx1 = 1/l.*(y1-y2).*u1;
 dy1 = 1/l.*(x2-x1).*u1;
 dx2 = 1/l.*(y1-y2).*u2;
@@ -16,20 +16,32 @@ ds = [dx1;dy1;dx2;dy2];
 end
 
 ODEFUN = @carddt;
-s0 = [0, 0, 1, 0];
-tspan = 20;
-[~,s]= ode45(ODEFUN, [0,tspan], s0);
+s0 = [0, 0, 1, 0];%åˆæ€
 
-set(gcf, 'doublebuffer', 'on')%·ÀÖ¹ÉÁË¸
+step = 0.1;%æ­¥é•¿
+t_start = 0;
+t_end = t_start+step;
 
+[~,s]= ode45(ODEFUN, [t_start,t_end], s0);
+% disp(size(t))
+% disp(size(s))
+%disp(t(size(t,1),1))
+
+set(gcf, 'doublebuffer', 'on')%é˜²æ­¢é—ªçƒ
 figure(1)
-for i =1:3*tspan
-    scatter(s(i,1), s(i,2))
+for i=1:100
+    scatter(s(size(s,1),1), s(size(s,1),2))
     hold on
-    scatter(s(i,3), s(i,4))
-    axis([-3 3 -3 3]); xlabel('X'); ylabel('Y');
+    scatter(s(size(s,1),3), s(size(s,1),4))
+    axis([-10 10 -10 10]); xlabel('X'); ylabel('Y')
     drawnow
     hold off
+
+    t_start = t_end;
+    t_end = t_start+step;
+    s_start = [s(size(s,1),1), s(size(s,1),2), s(size(s,1),3), s(size(s,1),4)];
+    
+    [~,s]= ode45(ODEFUN, [t_start,t_end], s_start);
 end
 
 end
