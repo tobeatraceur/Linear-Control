6fd01d8860c0bcad10b3e9ed6574813ea2aabf4b
li-chengkun <li.chengkun@qq.com>
2019-05-15 18:53:16 +0800

    Update SerialFuc.m

--- VehicleControl/SerialFuc.m
+++ VehicleControl/SerialFuc.m
@@ -2,19 +2,25 @@ function SerialFuc()
   delete(instrfindall)      % 关闭串口
   global s;
   %s = serial('com3');       %使用默认设置创建串口s
-  s = Bluetooth('1234', 5)   %连接蓝牙（名称，通道）
+  s = Bluetooth('BT04-A', 1)   %连接蓝牙（名称，通道）
   fopen(s);                 %打开串口
-  set(s,'BytesAvailableFcnMode','Terminator'); %设置中断触发方式
-  set(s,'Terminator','H');
+  %set(s,'BytesAvailableFcnMode','Terminator'); %设置中断触发方式
+  %set(s,'Terminator','H');
   %set(obj,'BytesAvailableFcnMode','byte'); 
   %set(obj,'BytesAvailableFcnCount', 240);
   %串口检测到输入缓存中到达了240个字符数据时,触发串口中断。
   
   s.BytesAvailableFcn =@ReceiveCallback;       % 定义中断响应函数对象
-  for k=1:1:5               % 每两秒发送字符串，循环五次
-      fprintf(s,'test');
-      pause(2);
-  end    
+
+while(1)
+    x=input('input:','s');
+    command = string(x);
+    disp('The commnd is:');disp(command);
+    fprintf(s,command);
+    if(command=='gxdsb')
+        break;
+    end
+end
 end
 function ReceiveCallback( obj,event)     %创建中断响应函数
    global s;
