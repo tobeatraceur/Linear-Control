7508c256307fde0f9654ff4ef51458925566c94b
li-chengkun <48325263+li-chengkun@users.noreply.github.com>
2019-05-26 21:40:28 +0800

    update for obstacles avoidance

--- Matlab/Controller.m
+++ Matlab/Controller.m
@@ -10,6 +10,10 @@ classdef Controller
         K_1 = 3;
         K_2 = 0.3;
         K_4 = 0.001;
+        K_r = 1000000;
+        
+        K_p = 0.2;
+        Repulsive_Distance = 200;
 
         L = 75.5;
 
@@ -34,9 +38,23 @@ classdef Controller
             y_T = target(2);
 
             obj.theta(end + 1) = rotation + obj.thetaFix;
+            
+            %Obstacles avoidance
+            Grad = [2*obj.K_p*(obj.x(end)-x_T), 2*obj.K_p*(obj.y(end)-y_T)];
+            for i = 1:size(obstacles,1)
+                x_R = obstacles(i,1);
+                y_R = obstacles(i,2);
+                if sqrt((obj.x(end)-x_R)^2 + (obj.y(end)-y_R)^2) < obj.Repulsive_Distance
+                    Grad = Grad + [((obj.x(end)-x_R)^2 + (obj.y(end)-y_R)^2)^(-3/2)*(x_R-obj.x(end)),...
+                        ((obj.x(end)-x_R)^2 + (obj.y(end)-y_R)^2)^(-3/2)*(y_R-obj.y(end))];        
+                end
+            end
 
-            v_d = 0.5 * sqrt((obj.x(end) - x_T)^2 + ( obj.y(end) - y_T )^2);
-            theta_dtheta = atan2((y_T - obj.y(end)), (x_T - obj.x(end)));
+            %v_d = 0.5 * sqrt((obj.x(end) - x_T)^2 + ( obj.y(end) - y_T )^2);
+            %theta_dtheta = atan2((y_T - obj.y(end)), (x_T - obj.x(end)));
+            v_d = sqrt(Grad(1)^2 + Grad(2)^2);
+            theta_dtheta = atan2(-Grad(2), -Grad(1));
+            
             w_d = obj.K_w * (2 * pi * round((obj.theta(end) - theta_dtheta)/(2 * pi)) - obj.theta(end) + theta_dtheta);
             x_e = cos(obj.theta(end) * (x_T - obj.x(end))) + sin(obj.theta(end) * (y_T - obj.y(end)));
             y_e = - sin(obj.theta(end) * (x_T - obj.x(end))) + cos(obj.theta(end) * (y_T - obj.y(end)));
